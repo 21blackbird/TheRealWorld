b77cdd746683372d91d1f2522538b34b71886e51
21blackbird <115868293+21blackbird@users.noreply.github.com>
2023-11-10 14:29:31 +0700

    first commit

---
+++ pages/Home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+    <div class="navbar">
+        <a href="./Home.php">Home</a>
+        <a href="./Cars.php">Car List</a>
+    </div>
+
+    <h1 id="top">Welcome to the Real World !</h1>
+
+
+</body>
+</html>
