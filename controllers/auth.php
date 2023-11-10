79a0ac9ad8178cafd31d3d2590627846a35732ac
21blackbird <115868293+21blackbird@users.noreply.github.com>
2023-11-10 14:52:46 +0700

    Seccond Commit

---
+++ controllers/auth.php
@@ -0,0 +1,55 @@
+<?php
+    session_start();
+    require "./connection.php";
+
+    function doLogin($username, $password) {
+    global $conn;
+    $query = "SELECT * FROM users WHERE username=? AND password=?;";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ss",$username,$password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result;
+    }
+
+    global $conn;
+
+    $query = "SELECT * FROM users WHERE username=? AND password=?;";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ss",$username,$password);
+    $stmt->execute();
+    $stmt->get_result();
+
+
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $login_result = doLogin($username, $password);
+
+        if ($login_result->num_rows == 1) {
+            $data = $login_result->fetch_assoc();
+
+           $_SESSION["success_message"] = "Welcome, $username";
+
+           $_SESSION['is_login'] = true;
+           $_SESSION['username'] = $data["username"];
+           $_SESSION["role"] = $data["role"];
+           $_SESSION["fullname"] = $data["fullname"];
+           $_SESSION["email"] = $data["email"];
+
+
+
+           header("Location: ../messages.php");
+
+        }
+
+        else {
+            $_SESSION["error_message"] = "Login Failed.";
+
+            header("Location: ../login.php?error=1");
+        }
+
+    }
+?>
